0f14e2760d43fb1b11ca473160739b4abf97e699
SayyidTM <sayyidkindi@gamil.com>
2019-04-05 01:16:04 +0700

    add

---
+++ contoh1/Model/Mahasiswa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace contoh1.Model
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Alamat { get; set; }
+    }
+}
